49ba44c7239c0631856ad1aa8a658560d064a3ff
penn <penn201500@gmail.com>
2024-03-21 00:11:33 +0800

    refactor: use express module to process http request and response

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const http = require("http");
+let express = require("express")
+let ourApp = express()
 
-let ourApp = http.createServer((req, res) => {
-    console.log(`the req url is ${req.url}`)
-    switch (req.url) {
-        case "/":
-            res.end('hello, this is a simple node.js server')
-            break
-        case "about":
-            res.end('hello, this is about page')
-            break;
-        default:
-            res.end('we cannot find the page you are looking for')
-    }
+ourApp.get('/', (req, res) => {
+    res.send('hello, this is a simple node.js server'
+    )
+})
+
+ourApp.get('/about', (req, res) => {
+    res.end('hello, this is about page')
+})
+
+ourApp.use((req, res) => {
+    res.status(404).send('we cannot find the page you are looking for')
+
 })
 
 ourApp.listen(3000)
