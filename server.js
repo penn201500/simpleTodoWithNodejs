d6967f99359914e7cf80930497a374490a82e8f8
penn <penn201500@gmail.com>
2024-03-20 21:17:14 +0800

    bugfix: update path check statement

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const http = require("http");
 
 let ourApp = http.createServer((req, res) => {
     console.log(`the req url is ${req.url}`)
-    if (req.url === "/") {
-        res.end('hello, this is a simple node.js server')
+    switch (req.url) {
+        case "/":
+            res.end('hello, this is a simple node.js server')
+            break
+        case "about":
+            res.end('hello, this is about page')
+            break;
+        default:
+            res.end('we cannot find the page you are looking for')
     }
-    if (req.url === "/about")
-    {
-        res.end('hello, this is about page')
-    }
-
-    res.end('we cannot find the page you are looking for')
 })
 
 ourApp.listen(3000)
