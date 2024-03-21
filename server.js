08c04c51e3cf8d90ecc133394d018e16d7d00142
penn <penn201500@gmail.com>
2024-03-21 23:38:24 +0800

    update html and server.js with some example data to display the simple todo page

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 let express = require("express")
+let path = require('path')
 let ourApp = express()
 
+ourApp.use(express.urlencoded({extended: false}))
+
 ourApp.get('/', (req, res) => {
-    res.send('hello, this is a simple node.js server'
-    )
+    res.sendFile(path.join(__dirname, 'todo.html'))
 })
 
 ourApp.get('/about', (req, res) => {
     res.end('hello, this is about page')
 })
+
+ourApp.post('/createItem', (req, res) => {
+    let itemValue = req.valueOf();
+
+        res.send(`
+        <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">Fake example item #3</span>
+            <div>
+                <button class="edit-me btn btn-secondary btn-sm me-1">Edit</button>
+                <button class="delete-me btn btn-danger btn-sm">Delete</button>
+            </div>
+        </li>
+        `)
+    }
+)
 
 ourApp.use((req, res) => {
     res.status(404).send('we cannot find the page you are looking for')
