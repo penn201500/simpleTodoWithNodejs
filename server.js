70ff8dcbaec39cdec320809d3a08822ef18270f1
penn <penn201500@gmail.com>
2024-03-22 22:23:29 +0800

    update server.js to fetch documents from mongodb and displayed on index.html page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 let express = require("express")
 let path = require('path')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const {MongoClient, ServerApiVersion} = require('mongodb');
 const {urlencoded} = require("express");
 require('dotenv').config()
 let ourApp = express()
@@ -29,7 +29,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
-        await client.db(dbName).command({ ping: 1 });
+        await client.db(dbName).command({ping: 1});
         db = await client.db(dbName);
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
         ourApp.listen(listeningPort, () => {
@@ -39,12 +39,54 @@ async function run() {
         console.error(e);
     }
 }
+
 run().catch(console.dir);
 
 ourApp.use(express.urlencoded({extended: false}))
 
-ourApp.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'todo.html'))
+ourApp.get('/', async (req, res) => {
+    const items = await db.collection('items').find().toArray((err, result) => {
+        if (err) return console.log(err)
+    })
+    res.send(`
+   <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple To-Do App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <h1 class="display-4 text-center py-1">To-Do App</h1>
+
+    <div class="jumbotron p-3 shadow-sm">
+        <form action="/createItem" method="POST">
+            <div class="d-flex align-items-center">
+                <input name="item" placeholder="Enter New To-Do" autofocus autocomplete="off" class="form-control me-3" type="text" style="flex: 1;">
+                <button class="btn btn-primary">Add New Item</button>
+            </div>
+        </form>
+    </div>
+
+    <ul class="list-group pb-5">
+    ${items.map(item => `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+            <span class="item-text">${item.text}</span>
+            <div>
+                <button class="edit-me btn btn-secondary btn-sm me-1">Edit</button>
+                <button class="delete-me btn btn-danger btn-sm">Delete</button>
+            </div>
+        </li>
+        `).join('')}
+    </ul>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html> 
+    `)
 })
 
 ourApp.get('/about', (req, res) => {
@@ -54,16 +96,7 @@ ourApp.get('/about', (req, res) => {
 ourApp.post('/createItem', async (req, res) => {
         let itemValue = req.body.item;
         await db.collection('items').insertOne({text: itemValue})
-        console.log(console.log('itemValue: ', itemValue))
-        res.send(`
-    <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
-        <span class="item-text">${itemValue}</span>
-        <div>
-            <button class="edit-me btn btn-secondary btn-sm me-1">Edit</button>
-            <button class="delete-me btn btn-danger btn-sm">Delete</button>
-        </div>
-    </li>
-    `)
+        res.redirect('/')
     }
 )
 
