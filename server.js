1691ccb180bbe78271541b67698a7fd2ae101335
penn <penn201500@gmail.com>
2024-03-24 14:47:55 +0800

    use winston module to output log info into files

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ let dbName = "TodoApp";
 let listeningPort = 3000;
 const uri = `mongodb+srv://${userName}:${password}@${dbHost}/?retryWrites=true&w=majority&appName=${appName}`;
 let db;
+const logger = require("./logger");
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,12 +32,12 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db(dbName).command({ping: 1});
         db = await client.db(dbName);
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        logger.info("Pinged your deployment. You successfully connected to MongoDB!")
         ourApp.listen(listeningPort, () => {
-            console.log(`server is running on port ${listeningPort}`);
+            logger.info(`server is running on port ${listeningPort}`)
         });
     } catch (e) {
-        console.error(e);
+        logger.error(e)
         await client.close(); // ensure the client closes on error
         process.exit(1); // exit with a failure code
     }
@@ -43,7 +45,7 @@ async function run() {
 
 //TODO: how to output log info into a file
 run().catch(async (e) => {
-        console.error(e);
+        logger.error(e)
         await client.close();
         process.exit(1);
     }
@@ -58,17 +60,16 @@ ourApp.use(express.json());
 
 function passwordProtected(req, res, next) {
     const auth = req.headers.authorization;
-    console.log('auth is', auth);
     if (auth && auth.startsWith("Basic ")) {
         const base64Credentials = auth.split(" ")[1];
         const credentials = Buffer.from(base64Credentials, "base64").toString("ascii");
         const [username, password] = credentials.split(":");
         if (username === "test" && password === "test") {
-           return next();
+            return next();
         }
     }
 
-    res.set("WWW-Authenticate", 'Basic realm="Simple Todo App"');
+    res.set("WWW-Authenticate", "Basic realm=\"Simple Todo App\"");
     res.status(401).send("Authentication required");
 }
 
@@ -82,7 +83,7 @@ ourApp.get("/get-items", async (req, res) => {
         const items = await db.collection(dbCollection).find().toArray();
         res.json(items);
     } catch (e) {
-        console.log(e);
+        logger.error(e)
         res.status(500).send("Error fetching items");
     }
 });
@@ -95,18 +96,23 @@ ourApp.post("/create-item", async (req, res) => {
         let itemValue = req.body.text;
         let info = await db.collection(dbCollection).insertOne({text: itemValue});
         res.json({text: itemValue, _id: info.insertedId});
+        logger.info(`Created an item ${info.insertedId}`);
     }
 );
 
 ourApp.post("/update-item", async (req, res) => {
     let itemValue = req.body.text;
-    await db.collection(dbCollection).findOneAndUpdate({_id: new ObjectId(req.body.id)}, {$set: {text: itemValue}});
+    let itemID = new ObjectId(req.body.id)
+    await db.collection(dbCollection).findOneAndUpdate({_id: itemID}, {$set: {text: itemValue}});
     res.send("success");
+    logger.info(`Updated an item ${itemID}`);
 });
 
 ourApp.post("/delete-item", async (req, res) => {
-    await db.collection(dbCollection).deleteOne({_id: new ObjectId(req.body.id)});
+    let itemID = new ObjectId(req.body.id)
+    await db.collection(dbCollection).deleteOne({_id: itemID});
     res.send("success");
+    logger.info(`Deleted an item ${itemID}`);
 });
 ourApp.use((req, res) => {
     res.status(404).send("we cannot find the page you are looking for");
@@ -114,14 +120,14 @@ ourApp.use((req, res) => {
 
 // close the MongoDB connection when the Node.js process ends
 process.on("SIGINT", async () => {
-        console.log("Closing MongoDB connection due to app termination");
+        logger.info("Closing MongoDB connection due to app termination");
         await client.close();
         process.exit(0);
     }
 );
 
 process.on("SIGTERM", async () => {
-        console.log("Closing MongoDB connection due to app normal termination");
+        logger.info("Closing MongoDB connection due to app normal termination");
         await client.close();
         process.exit(0);
     }
